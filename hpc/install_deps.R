0f86fd2c6bbc4ca24087ebda2c7eb712f982d86a
Xiyuan Guo <tommyguo039@gmail.com>
2026-05-30 21:10:34 -0400

    fix(hpc): add missing deps mice, ranger, missRanger to install script

--- hpc/install_deps.R
+++ hpc/install_deps.R
@@ -3,7 +3,8 @@ lib_path <- "~/R/rivanna-lib"
 dir.create(lib_path, recursive = TRUE, showWarnings = FALSE)
 .libPaths(c(lib_path, .libPaths()))
 
-dependencies <- c("missForest", "lavaan", "Rcpp", "RcppArmadillo", "MASS")
+dependencies <- c("missForest", "missRanger", "mice", "ranger",
+                  "lavaan", "Rcpp", "RcppArmadillo", "MASS")
 
 for (pkg in dependencies) {
   if (!requireNamespace(pkg, quietly = TRUE)) {
